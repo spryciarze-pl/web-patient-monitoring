256b05287599b7d7388d849862997f2465353708
barcic2948 <92156137+barcic2948@users.noreply.github.com>
2024-02-05 22:13:07 +0100

    Feature/20240205 workv3 test rebase (#16)
    * Feature/20240121 db class and view fixes (#9)
    
    * Login service via the DB user list (#2)
    
    * Jajkon - Login through DB
    
    * Jajkon 2 - Add example credentials to log in
    
    * Added current page title displaying at the top of the page
    
    * Added User Profile (#4)
    
    * view pannel done (#6)
    
    * view done
    
    * rebase and fix
    
    ---------
    
    Co-authored-by: kando-123 <kamilgwozdz001@gmail.com>
    
    * Deleted you know what (#7)
    
    * PanelView done (#8)
    
    * WIP 1
    
    * Working user in session storage
    
    ---------
    
    Co-authored-by: Zbigniew Serafin <zbysiu1501@gmail.com>
    Co-authored-by: Zbigniew Serafin <92160264+zbysiukaczucha@users.noreply.github.com>
    Co-authored-by: kando-123 <kamilgwozdz001@gmail.com>
    Co-authored-by: Szymon-Kozlowski <127625799+Szymon-Kozlowski@users.noreply.github.com>
    
    * Login service via the DB user list (#2)
    
    * Jajkon - Login through DB
    
    * Jajkon 2 - Add example credentials to log in
    
    * Added User Profile (#4)
    
    * PanelView done (#8)
    
    * Feature/20240121 db class and view fixes (#10)
    
    * WIP 1
    
    * Working user in session storage
    
    * Activity view complete
    
    ---------
    
    Co-authored-by: Zbigniew Serafin <zbysiu1501@gmail.com>
    Co-authored-by: Zbigniew Serafin <92160264+zbysiukaczucha@users.noreply.github.com>
    Co-authored-by: kando-123 <kamilgwozdz001@gmail.com>
    Co-authored-by: Szymon-Kozlowski <127625799+Szymon-Kozlowski@users.noreply.github.com>

--- src/main/java/se/db/repository/AppointmentRepository.java
+++ src/main/java/se/db/repository/AppointmentRepository.java
@@ -2,10 +2,10 @@ package se.db.repository;
 
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import se.db.model.Appointment;
 
 import java.util.List;
